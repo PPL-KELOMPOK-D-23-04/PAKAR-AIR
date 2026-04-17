b28cb3889cd05754d67ffa46bbb524a7cb6513f2
adamyudhistiramuhtar-byte <adamyudhistiramuhtar@gmail.com>
2026-04-17 15:13:17 +0700

    PKD20TPA-5 feat(ui): create image uploader component

--- frontend/src/components/analysis/ImageUploader.vue
+++ frontend/src/components/analysis/ImageUploader.vue
@@ -1,3 +1,22 @@
+<script setup>
+import { useFileUpload } from '@/composables/useFileUpload'
+import { useAnalysisStore } from '@/stores/analysisStore'
+
+const store = useAnalysisStore()
+const { previewUrl, handleFileChange } = useFileUpload()
+
+const onChange = (e) => {
+  handleFileChange(e)
+  store.setImage(e.target.files[0])
+}
+</script>
+
 <template>
-  <input type="file" />
+  <div>
+    <h3>Upload Citra Air</h3>
+
+    <input type="file" @change="onChange" />
+
+    <img v-if="previewUrl" :src="previewUrl" width="200" />
+  </div>
 </template>
