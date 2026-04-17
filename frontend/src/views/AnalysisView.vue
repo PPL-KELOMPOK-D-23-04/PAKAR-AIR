4e65f34f5aa15928a3f48f6dc690a76385f7a496
adamyudhistiramuhtar-byte <adamyudhistiramuhtar@gmail.com>
2026-04-17 15:14:44 +0700

    PKD20TPA-5 feat(view): integrate upload and manual form into analysis page

---
+++ frontend/src/views/AnalysisView.vue
@@ -0,0 +1,13 @@
+<script setup>
+import ImageUploader from '@/components/analysis/ImageUploader.vue'
+import ManualDataForm from '@/components/analysis/ManualDataForm.vue'
+</script>
+
+<template>
+  <div>
+    <h1>Analisis Kualitas Air</h1>
+
+    <ImageUploader />
+    <ManualDataForm />
+  </div>
+</template>
