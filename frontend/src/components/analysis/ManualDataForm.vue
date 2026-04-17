4bfae93e2722e9f7972b66ccd27f8959d5d12736
adamyudhistiramuhtar-byte <adamyudhistiramuhtar@gmail.com>
2026-04-17 15:14:24 +0700

    PKD20TPA-5 feat(ui): create manual data form component

---
+++ frontend/src/components/analysis/ManualDataForm.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { reactive, watch } from 'vue'
+import { useAnalysisStore } from '@/stores/analysisStore'
+
+const store = useAnalysisStore()
+
+const form = reactive({
+  warna: '',
+  bau: '',
+  sumber: '',
+  ph: '',
+  suhu: ''
+})
+
+watch(form, () => {
+  store.setManualData(form)
+}, { deep: true })
+</script>
+
+<template>
+  <div>
+    <h3>Input Data Manual</h3>
+
+    <input v-model="form.warna" placeholder="Warna" />
+    <input v-model="form.bau" placeholder="Bau" />
+    <input v-model="form.sumber" placeholder="Sumber" />
+    <input v-model="form.ph" placeholder="pH" />
+    <input v-model="form.suhu" placeholder="Suhu" />
+  </div>
+</template>
