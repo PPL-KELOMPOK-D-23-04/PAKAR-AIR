60dc82d4b1313d9eaaaeb48c7ac9301a761f7e5c
Christian Toga <christiantoga82@gmail.com>
2026-04-29 18:24:49 +0700

    Port changes

--- server/run.py
+++ server/run.py
@@ -5,5 +5,5 @@ from dotenv import load_dotenv
 load_dotenv()
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8000))
     uvicorn.run("app.main:app", host="0.0.0.0", port=port, reload=True)
