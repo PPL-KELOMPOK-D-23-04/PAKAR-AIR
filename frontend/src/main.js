5669b4e4d48f8cabf21a4173e3fa112bd20d25e8
adamyudhistiramuhtar-byte <adamyudhistiramuhtar@gmail.com>
2026-05-06 17:09:22 +0700

    PKD20TPA-2 refactor: penyesuaian routing dan inisialisasi aplikasi

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,13 +1,17 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { useAuthStore } from '@/stores/authStore'
 
 import App from './App.vue'
 import router from './router'
 import './assets/styles/main.css'
 
 const app = createApp(App)
-
-app.use(createPinia())
+const pinia = createPinia()
+app.use(pinia)
 app.use(router)
+
+const authStore = useAuthStore()
+authStore.initAuth()
 
 app.mount('#app')
